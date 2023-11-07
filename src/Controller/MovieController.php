4d352fa0caff51fafd2fda3a6eed02a166474e79
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-11-07 17:11:55 +0100

    Twig - movie base

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/movie')]
+class MovieController extends AbstractController
+{
+    #[Route('', name: 'app_movie_index')]
+    public function index(): Response
+    {
+        return $this->render('movie/index.html.twig', [
+            'controller_name' => 'MovieController',
+        ]);
+    }
+
+    #[Route('/{id<\d+>}', name: 'app_movie_show', methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        $movie = [
+            'id' => $id,
+            'title' => 'Star Wars - Episode IV : A New Hope',
+            'country' => 'United States',
+            'released' => new \DateTimeImmutable('25/05/1977'),
+            'genre' => [
+                'Action',
+                'Adventure',
+                'Fantasy',
+            ],
+        ];
+
+        return $this->render('movie/show.html.twig', [
+            'movie' => $movie,
+        ]);
+    }
+}
