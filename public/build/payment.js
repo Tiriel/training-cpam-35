05f862ac2c521f164668c15130888fd1d38a7a4b
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-11-08 10:27:23 +0100

    Add build folder

---
+++ public/build/payment.js
@@ -0,0 +1,21 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["payment"],{
+
+/***/ "./assets/payment.js":
+/*!***************************!*\
+  !*** ./assets/payment.js ***!
+  \***************************/
+/***/ (() => {
+
+document.querySelector('h1').addEventListener('click', function (e) {
+  console.log('Clicked!');
+});
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/payment.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGF5bWVudC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBQSxRQUFRLENBQUNDLGFBQWEsQ0FBQyxJQUFJLENBQUMsQ0FBQ0MsZ0JBQWdCLENBQUMsT0FBTyxFQUFFLFVBQVVDLENBQUMsRUFBRTtFQUNoRUMsT0FBTyxDQUFDQyxHQUFHLENBQUMsVUFBVSxDQUFDO0FBQzNCLENBQUMsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2Fzc2V0cy9wYXltZW50LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJ2gxJykuYWRkRXZlbnRMaXN0ZW5lcignY2xpY2snLCBmdW5jdGlvbiAoZSkge1xuICAgIGNvbnNvbGUubG9nKCdDbGlja2VkIScpO1xufSlcbiJdLCJuYW1lcyI6WyJkb2N1bWVudCIsInF1ZXJ5U2VsZWN0b3IiLCJhZGRFdmVudExpc3RlbmVyIiwiZSIsImNvbnNvbGUiLCJsb2ciXSwic291cmNlUm9vdCI6IiJ9
