93de75d6e407580ded54a8be70aa4883d2e7c8bb
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-11-09 09:23:20 +0100

    Form base for Movie

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Form\MovieType;
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,6 +27,27 @@ class MovieController extends AbstractController
     {
         return $this->render('movie/show.html.twig', [
             'movie' => $movie,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_movie_new', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_movie_edit', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, ?Movie $movie = null): Response
+    {
+        $movie ??= new Movie();
+        $form = $this->createForm(MovieType::class, $movie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($movie);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_movie_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('movie/new.html.twig', [
+            'movie' => $movie,
+            'form' => $form,
         ]);
     }
 }
--- src/Form/MovieType.php
+++ src/Form/MovieType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Movie;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +16,18 @@ class MovieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('poster')
-            ->add('country')
-            ->add('releasedAt')
-            ->add('plot')
-            ->add('price')
+            ->add('title', TextType::class)
+            ->add('poster', TextType::class)
+            ->add('country', TextType::class)
+            ->add('releasedAt', DateType::class, [
+                'input' => 'datetime_immutable',
+                'widget' => 'single_text',
+            ])
+            ->add('plot', TextareaType::class)
+            ->add('price', MoneyType::class, [
+                'required' => false,
+                'scale' => 100,
+            ])
         ;
     }
 
